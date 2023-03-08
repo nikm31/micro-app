e5e67dcc9f384ec69367ed8308ee260fc7b470f8
nmatvienko <firewather@gmail.com>
2023-03-08 17:40:57 +0300

    111

---
+++ eureka-client/src/main/java/ru/microapp/eurekaclient/controller/TestController.java
@@ -0,0 +1,23 @@
+package ru.microapp.eurekaclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/main") // корневой URI
+public class TestController {
+
+    @Value("${eureka.instance.instance-id}")
+    private String id;
+
+    @Value("${tmpVar}")
+    private String tmpVar;
+
+    @GetMapping("/test")
+    public String test(){
+        return id + " : " + tmpVar;
+    }
+
+}
